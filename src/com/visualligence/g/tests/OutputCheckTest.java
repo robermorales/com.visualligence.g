af7e33c7a0c9e676f642446cf597505cf72d2fc1
Rober Morales-Chaparro <rober.morales@gmail.com>
2012-04-26 13:52:28 +0200

    model testing refactoring

--- src/com/visualligence/g/tests/OutputCheckTest.java
+++ src/com/visualligence/g/tests/OutputCheckTest.java
@@ -5,12 +5,16 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FilenameFilter;
+
+import junit.framework.JUnit4TestCaseFacade;
 
 import org.eclipse.xtext.junit4.InjectWith;
 import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipse.xtext.junit4.util.ParseHelper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import com.google.inject.Inject;
 import com.visualligence.g.VMLInjectorProvider;
@@ -24,21 +28,43 @@ public class OutputCheckTest {
 	ParseHelper<Module> parser;
 
 	@Test
-	public void imo() throws Exception {
-		File dir = new File("i-m-o");
+	public void model_i_o() throws Exception {
+		File dir = new File("models-io");
 		assertTrue(dir.isDirectory());
 		for (File test : dir.listFiles()) {
-			assertTrue(test.isDirectory());
+			if (test.isDirectory()) {
+				File body = new File(dir, test.getName() + ".vml");
 
-			File model = new File(test, test.getName() + ".vml");
-			byte[] buffer = new byte[(int) model.length()];
-			new FileInputStream(model).read(buffer);
+				byte[] buffer_vml = new byte[(int) body.length()];
+				new FileInputStream(body).read(buffer_vml);
 
-			Module module = parser.parse(new String(buffer));
-			assertNotNull(module);
+				Module module = parser.parse(new String(buffer_vml));
+				assertNotNull(module);
 
-			// File[] inputs = new File(test, "input").listFiles();
-			// File[] outputs = new File(test, "output").listFiles();
+				File inputs = new File(test, "input");
+				File outputs = new File(test, "output");
+				for (File input : inputs.listFiles()) {
+					File output;
+					try {
+						output = new File(outputs, input.getName());
+
+						byte[] buffer_in = new byte[(int) body.length()];
+						new FileInputStream(input).read(buffer_in);
+
+						byte[] buffer_out = new byte[(int) body.length()];
+						new FileInputStream(output).read(buffer_out);
+
+						
+						
+//						System.out.println(new String(buffer_in));
+//						System.out.println(new String(buffer_out));
+//						System.out.println(new String(buffer_vml));
+
+					} catch (Exception e) {
+
+					}
+				}
+			}
 		}
 	}
 }
